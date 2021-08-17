a1ba99b6b95983be98fed748bd5c04e0696c1e7f
vernaud <renaud.verdon2021@campus-eni.fr>
2021-08-17 22:19:46 +0200

    feat(fixture): add Participant

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Participant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $participant = new Participant();
+        $participant->setNom('DELORD');
+        $participant->setPrenom('Matthieu');
+        $participant->setPseudo('superDev44');
+        $participant->setTelephone('0654986532');
+        $participant->setEmail('superdev@eni.fr');
+        $participant->setPassword($this->passwordHasher->hashPassword(
+            $participant,
+            'pass'
+        ));
+        $participant->setRoles(['ROLE_USER']);
+        $participant->setActif(true);
+
+        $manager->persist($participant);
 
         $manager->flush();
     }
