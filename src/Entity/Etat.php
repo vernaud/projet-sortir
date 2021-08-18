0b46c37dfba9ba291154c8092221fb76a3901497
Antoine Sorin <contact@antoine-sorin.com>
2021-08-18 10:21:19 +0200

    Création Entity Sortie et Etat

---
+++ src/Entity/Etat.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtatRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtatRepository::class)
+ */
+class Etat
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Sortie::class, mappedBy="Sortie")
+     */
+    private $Sortie;
+
+    public function __construct()
+    {
+        $this->Sortie = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Sortie[]
+     */
+    public function getSortie(): Collection
+    {
+        return $this->Sortie;
+    }
+
+    public function addSortie(Sortie $sortie): self
+    {
+        if (!$this->Sortie->contains($sortie)) {
+            $this->Sortie[] = $sortie;
+            $sortie->setSortie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSortie(Sortie $sortie): self
+    {
+        if ($this->Sortie->removeElement($sortie)) {
+            // set the owning side to null (unless already changed)
+            if ($sortie->getSortie() === $this) {
+                $sortie->setSortie(null);
+            }
+        }
+
+        return $this;
+    }
+}
