c6f944f67fd594b95763f474327adf8743007c5a
vernaud <renaud.verdon2021@campus-eni.fr>
2021-08-20 15:17:53 +0200

    chore(view): input date pour dateLimitInscription
    ajout des types sur les champs de formulaires

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -6,6 +6,11 @@ use App\Entity\Lieu;
 use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,19 +18,33 @@ class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // todo compléter en ajoutant les types de champs et les options
+
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
+            ->add('nom', TextType::class, [
+                'label'=>"Nom"
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label'=>'Date et heure de la sortie'
+            ])
+            ->add('duree', IntegerType::class, [
+                'label'=>'Durée (en minutes)'
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'html5' => true,
+                'widget' => 'single_text',
+                'label'=>'Date limite d\'inscription'
+            ])
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label'=>'Nombre de places'
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label'=>'Description'
+            ])
 
             ->add('lieu',EntityType::class, [
                 'class'=>Lieu::class,
                 'choice_label'=>'nom',
-                'label'=>'LieuFixtures'])
+                'label'=>'Lieu'])
 
         ;
     }
